c7253811ba3300054a1d3cee2bd2e8d1daf2e3c3
fengyuli <fengyuli2002@gmail.com>
2024-10-18 14:12:46 -0700

    chore: Update gitignore and metric logger configuration

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3
+CUDA_VISIBLE_DEVICES=5
 
 # Clear prev output
 rm -rf output
 mkdir output
 
-tune run --nproc_per_node 4 /lfs/local/0/fengyuli/cs229-project/full_finetune_distributed.py --config /lfs/local/0/fengyuli/cs229-project/configs/config.yaml
+tune run --nproc_per_node 4 full_finetune_distributed --config configs/config.yaml
