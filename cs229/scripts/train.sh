4a8e5a2caa9eca2fd3b5b49aab80ea1d4c41e4b8
fengyuli <fengyuli2002@gmail.com>
2024-11-21 17:16:31 -0800

    Config for 1b

--- cs229/scripts/train.sh
+++ cs229/scripts/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=7
+CUDA_VISIBLE_DEVICES=6
 
 # Clear prev output
-rm -rf /lfs/local/0/fengyuli/output/cs229/
-mkdir /lfs/local/0/fengyuli/output/cs229/
+# rm -rf /lfs/local/0/fengyuli/output/cs229/
+# mkdir /lfs/local/0/fengyuli/output/cs229/
 
-tune run --nproc_per_node 1 -m cs229.full_finetune_distributed --config /lfs/local/0/fengyuli/cs229-project/cs229/configs/config.yaml
+tune run --nproc_per_node 1 -m cs229.full_finetune_distributed --config /lfs/local/0/fengyuli/cs229-project/cs229/configs/config_1b.yaml
