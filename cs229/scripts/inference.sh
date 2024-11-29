11e2b0065ec6117372e1c5c896071e4e121a845f
fengyuli <fengyuli2002@gmail.com>
2024-11-29 10:13:03 -0800

    Refactor CUDA_VISIBLE_DEVICES assignment in inference.sh

--- cs229/scripts/inference.sh
+++ cs229/scripts/inference.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=7
-
-NGPUS=1
-PYTHONPATH=$(git rev-parse --show-toplevel)
+export CUDA_VISIBLE_DEVICES=7
 
 torchrun --nproc_per_node=1 -m cs229.util --testall_local
 # torchrun --nproc_per_node=1 -m cs229.util --gendata
